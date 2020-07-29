92f56a74cfc85bdb289ed76c0f30b3e2f738ba10
Alex Fernez <afernez@umd.edu>
2020-07-28 23:22:16 -0400

    cleanup code for plot_phoebe_datamc

--- src/rdx/plot_phoebe_datamc.cxx
+++ src/rdx/plot_phoebe_datamc.cxx
@@ -1,4 +1,4 @@
-#include <fstream>
+ddmu#include <fstream>
 #include <iostream>
 #include <vector>
 #include <ctime>
@@ -23,12 +23,10 @@
 using namespace std;
 using namespace PlotOptTypes;
 
-// ** I could add ddtau_u, ddtau_d, ddmu_u, ddmu_d, ddmu_s types: should these all be differentiated? No ddtau_s??
-// Maybe, at least, you should differentiate between ddtau and ddmu (using flagTauonicD)... I will do this
-// and I'll call the types with mu dxx (this is based on table 1 pg 5 of ANA note) and those with tau dd... I
-// may change this in the future. I've DELETED THE DSS TYPE MANUEL CREATED, replacing it with dss_mu and dss_tau.
-// This may change, too...
-enum class eventType {data, dsptau, dspmu, dss_mu, dss_tau, dd, dxx, unknown}; // ADD ANY OTHER EVENT TYPES HERE
+
+// Should dssmu && dsstau and ddmu && ddtau be grouped together (just dss and dd types)?
+enum class eventType {data, dsptau, dspmu, dssmu, dsstau, ddtau, ddmu, unknown}; // ADD ANY OTHER EVENT TYPES HERE
+
 
 eventType getType(double isData, double DstIDprod, double IDprod, float muPID, double flagDstSB,
                   double flagtaumu, double JustDst, double DstOk, int Btype, double flagBmu,
@@ -42,22 +40,16 @@ eventType getType(double isData, double DstIDprod, double IDprod, float muPID, d
   // ADD CONDITIONS FOR DSS, DD, AND ANY OTHER EVENT TYPES YOU NEED TO LOOK AT
   else if(isData == 0. &&  DstOk > 0. && muPID == 1. && flagTauonicD > 0.
           && (Btype==511 || Btype==521))
-    return eventType::dd;
+    return eventType::ddtau;
   else if(isData == 0. &&  DstOk > 0. && muPID == 1. && flagDoubleD > 0. && flagTauonicD < 1.
           && (Btype==511 || Btype==521 || Btype==531))
-    return eventType::dxx;
-  /* Examples of type criteria you could set if being more specific about DD/DXX' events. Note that Phoebe
-  seems to not check flagDoubleD for the ddtau types, and she seemingly doesn't consider a ddtau_s...
-  else if(isData == 0. &&  DstOk > 0. && muPID == 1. && flagDoubleD > 0. && Btype==531 && flagTauonicD < 1.)
-    return eventType::ddmu_s;
-  else if(isData == 0. &&  DstOk > 0. && muPID == 1. && flagTauonicD > 0. && Btype==511) // no flagDoubleD??
-    return eventType::ddtau_d;
-  */
+    return eventType::ddmu;
 
   // add conditionals for D**...
 
   return eventType::unknown;
 }
+
 
 
 int main(){
@@ -83,9 +75,10 @@ int main(){
                                           // provided as argument for histograms, all plot types should be created
                                           // as a separate pdf
 
-  //////// Signal, normalization, D** processes for mmiss2 plot
+  //////// Signal, normalization, D** and DD processes for plots
   string repofolder = "ntuples/";
   string ntuplefile = "ref-rdx-run1/Dst-mix/Dst--20_07_02--mix--all--2011-2012--md-mu--phoebe.root";
+
 
   NamedFunc event_type("event_type",[&](const Baby &b){
                                       return static_cast<double>(getType(b.isData(), b.DstIDprod(), b.IDprod(), b.muPID(), b.flagDstSB(),
@@ -98,8 +91,11 @@ int main(){
                                                        b.flagtaumu(), b.JustDst(), b.DstOk(), b.Btype(), b.flagBmu(), b.Y_BKGCAT(),
                                                        b.flagTauonicD(), b.flagDoubleD());
                               if(type == eventType::data) return 1.;
-                              else return b.FFweight()*b.mcWeight(); // does this have to be modified?
+                              else return b.FFweight()*b.mcWeight(); // this should be modified to account for weights
+                                                                     // changing in Phoebe's code... probably better to
+                                                                     // define individual weight functions, even
                             });
+
 
   vector<shared_ptr<Process> > procs;
   procs.push_back(Process::MakeShared<Baby_phoebe_dsp>("Data",Process::Type::data, colors("data"),
@@ -111,26 +107,29 @@ int main(){
   procs.push_back(Process::MakeShared<Baby_phoebe_dsp>("B #rightarrow D*^{+} #mu #nu",Process::Type::background, colors("dspmu"),
                                                        set<string>({repofolder+ntuplefile}),
                                                        event_type == static_cast<double>(eventType::dspmu)));
-  // ADD ANY OTHER PROCESSES HERE, eg. MC B->D** tau nu, ASSUMEDLY AS BACKGROUND PROCESS TYPES
-  // this first process might have to be modified? also the ones following it too...
+  // ADD ANY OTHER PROCESSES HERE, eg. MC B->D** tau nu, AS BACKGROUND PROCESS TYPES
+  // Don't include unknown processes? They are concentrated heavily at large values (of mmiss2, El, q2), and they affect
+  // the weights of the other (Monte Carlo) processes
   /*procs.push_back(Process::MakeShared<Baby_phoebe_dsp>("Unknown process",Process::Type::background, colors("purple"),
                                                        set<string>({repofolder+ntuplefile}),
                                                        event_type == static_cast<double>(eventType::unknown)));*/
-  // Are reaction descriptions for DD and DXX' modes correct? I'm basing on table 1 pg 5 of ANA note
+  // Are reaction descriptions for ddtau and ddmu modes correct? I'm basing on table 18 pg 91 of ANA note
   procs.push_back(Process::MakeShared<Baby_phoebe_dsp>("B #rightarrow D*^{+}(D_{s}^{-} #rightarrow #tau #nu) X",
                                                        Process::Type::background, colors("dd"),
                                                        set<string>({repofolder+ntuplefile}),
-                                                       event_type == static_cast<double>(eventType::dd)));
+                                                       event_type == static_cast<double>(eventType::ddtau)));
   procs.push_back(Process::MakeShared<Baby_phoebe_dsp>("B #rightarrow D*(X_{c} #rightarrow #mu #nu X') X",
                                                        Process::Type::background, colors("orange"),
                                                        set<string>({repofolder+ntuplefile}),
-                                                       event_type == static_cast<double>(eventType::dxx)));
-  // include process (actually, 2 I think) for D** feed down
+                                                       event_type == static_cast<double>(eventType::ddmu)));
+  // include processes for D** feed down
 
 
 
   NamedFunc isocuts = "1"; // ADD HERE CUTS FOR ISO SAMPLE, FOR EXAMPLE. (does this have to be modified?)
-  // CREATE NAMEDFUNC FOR OTHER TYPES OF CUTS
+  // CREATE NAMEDFUNCS FOR OTHER TYPES OF CUTS
+
+
   PlotMaker pm;
   pm.Push<Hist1D>(Axis(40, -2, 10, "m_nu1", "m_{miss}^{2} [GeV^{2}]"), isocuts, procs, plottypes,
                   vector<NamedFunc>({weight}));
