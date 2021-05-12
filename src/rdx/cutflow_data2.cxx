500a05c27a74e22a675da82874617af136bb3332
Manuel Franco Sevilla <manuelf@umd.edu>
2021-05-11 22:49:22 -0400

    Synced cutflow cuts with Yipeng's

--- src/rdx/cutflow_data2.cxx
+++ src/rdx/cutflow_data2.cxx
@@ -74,10 +74,10 @@ int main(int argc, char *argv[]){
 
   string repofolder = "ntuples/";
   vector<shared_ptr<Process> > procs;
-  procs.push_back(Process::MakeShared<Baby_run1_mc_b0>("D^{*+}\\mu\\nu~2012", Process::Type::background, colors("run1"),
-                                                set<string>({repofolder+"0.9.3-production_for_validation/Dst_D0-mc/Dst_D0--21_01_30--mc--MC_2012_Beam4000GeV-2012-MagDown-Nu2.5-Pythia8_Sim08e_Digi13_Trig0x409f0045_Reco14a_Stripping20Filtered_11574020_DSTTAUNU.SAFESTRIPTRIG.DST.root"}), trig_run1));
-  procs.push_back(Process::MakeShared<Baby_run2_mc_b0>("D^{*+}\\mu\\nu~2016", Process::Type::background, colors("data"),
-                                                set<string>({repofolder+"0.9.4-trigger_emulation/Dst_D0-mc/Dst_D0--21_04_21--mc--MC_2016_Beam6500GeV-2016-MagDown-Nu1.6-25ns-Pythia8_Sim09j_Trig0x6139160F_Reco16_Turbo03a_Filtered_11574021_D0TAUNU.SAFESTRIPTRIG.DST.root"}), trig_run2));
+  procs.push_back(Process::MakeShared<Baby_run1_mc_b0>("Data~MD~2011", Process::Type::background, colors("run1"),
+                                                set<string>({repofolder+"0.9.2-2011_production/Dst_D0-std/Dst_D0--20_10_12--std--LHCb_Collision11_Beam3500GeV-VeloClosed-MagDown_Real_Data_Reco14_Stripping21r1_90000000_SEMILEPTONIC.DST.root"}), trig_run1));
+  procs.push_back(Process::MakeShared<Baby_run2_mc_b0>("Data~MD~2016", Process::Type::background, colors("data"),
+                                                set<string>({repofolder+"0.9.4-trigger_emulation/Dst_D0-std/Dst_D0--21_04_27--std--LHCb_Collision16_Beam6500GeV-VeloClosed-MagDown_Real_Data_Reco16_Stripping28r1_90000000_SEMILEPTONIC.DST.root"}), trig_run2));
 
 
 
@@ -140,7 +140,7 @@ int main(int argc, char *argv[]){
  
 
   PlotMaker pm;
-  pm.Push<Table>("cutflow", table_rows,procs,0).TotColumn("Ratio", 614577/1500395.*0.23/0.07*0.080/0.059); // BKK events for the two MC samples
+  pm.Push<Table>("cutflow", table_rows,procs,0).TotColumn("Ratio", 596.061/841.275/2); // Lumis and xsec
   
   pm.min_print_ = true;
   pm.MakePlots(1);
