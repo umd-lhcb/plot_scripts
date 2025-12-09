badacaaca07805a439341aae7ab3b957f8c0212b
Manuel Franco Sevilla <manuelf@umd.edu>
2025-12-09 10:31:37 -0500

    Added more heavy D** plots

--- src/rdx/plot_heavy_dss.cxx
+++ src/rdx/plot_heavy_dss.cxx
@@ -61,7 +61,9 @@ int main(){
 
   string ntpD0 = "ntuples/0.9.17-all_years/2016/Dstst_heavy/DststHMuD0-12675011-MagDown/D0--25_12_02--mc--12675011--2016--md--tracker_only.root";
   //string ntpDst = "ntuples/0.9.17-all_years/2016/Dstst_heavy/DststHMuDst-11676012-MagDown/Dst--25_12_02--mc--11676012--2016--md--tracker_only.root";
-  string ntpDst = "ntuples/0.9.17-all_years/2016/Dstst_heavy/DststHMuDst-11676012-MagDown/D0--25_12_02--mc--11676012--2016--md--tracker_only.root";
+  string ntpDst = "ntuples/0.9.17-all_years/2016/Dstst_heavy/DststHMuDst-11676012-MagDown/Dst--25_12_02--mc--11676012--2016--md--tracker_only.root";
+  string ntpDst_D0 = "ntuples/0.9.17-all_years/2016/Dstst_heavy/DststHMuDst-11676012-MagDown/D0--25_12_02--mc--11676012--2016--md--tracker_only.root";
+  string ntpDst0 = "ntuples/0.9.17-all_years/2016/Dstst_heavy/DststHMuDst0-12875440-MagDown/D0--25_12_02--mc--12875440--2016--md--tracker_only.root";
   vector<shared_ptr<Process> > procs;
   procs.push_back(Process::MakeShared<Baby_rdx917>
                   ("D**(2580) #rightarrow D^{0}", Process::Type::background, colors("purple"),
@@ -74,26 +76,86 @@ int main(){
                    set<string>({ntpD0}), globalCuts + " && truthmatch%100 == 30")); 
   procs.push_back(Process::MakeShared<Baby_rdx917>
                   ("D**(2978) #rightarrow D^{0}", Process::Type::background, colors("darkblue"),
-                   set<string>({ntpD0}), globalCuts + " && truthmatch%100 == 40")); 
-  procs.push_back(Process::MakeShared<Baby_rdx917>
+                   set<string>({ntpD0}), globalCuts + " && truthmatch%100 == 40"));
+  
+  vector<shared_ptr<Process> > procsDst;
+  procsDst.push_back(Process::MakeShared<Baby_rdx917>
                   ("D**(2580) #rightarrow D^{*+}", Process::Type::background, colors("purple"),
-                   set<string>({ntpDst}), globalCuts + " && truthmatch%100 == 10",2)); 
-  procs.push_back(Process::MakeShared<Baby_rdx917>
+                   set<string>({ntpDst}), globalCuts + " && truthmatch%100 == 10")); 
+  procsDst.push_back(Process::MakeShared<Baby_rdx917>
                   ("D**(2640) #rightarrow D^{*+}", Process::Type::background, colors("red"),
-                   set<string>({ntpDst}), globalCuts + " && truthmatch%100 == 20",2)); 
-  procs.push_back(Process::MakeShared<Baby_rdx917>
+                   set<string>({ntpDst}), globalCuts + " && truthmatch%100 == 20")); 
+  procsDst.push_back(Process::MakeShared<Baby_rdx917>
                   ("D**(2737) #rightarrow D^{*+}", Process::Type::background, colors("green"),
-                   set<string>({ntpDst}), globalCuts + " && truthmatch%100 == 30",2)); 
-  procs.push_back(Process::MakeShared<Baby_rdx917>
+                   set<string>({ntpDst}), globalCuts + " && truthmatch%100 == 30")); 
+  procsDst.push_back(Process::MakeShared<Baby_rdx917>
                   ("D**(2978) #rightarrow D^{*+}", Process::Type::background, colors("darkblue"),
-                   set<string>({ntpDst}), globalCuts + " && truthmatch%100 == 40",2)); 
+                   set<string>({ntpDst}), globalCuts + " && truthmatch%100 == 40")); 
+
+  vector<shared_ptr<Process> > procsDst_D0;
+  procsDst_D0.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2580) #rightarrow D^{*+}", Process::Type::background, colors("purple"),
+                   set<string>({ntpDst_D0}), globalCuts + " && truthmatch%100 == 10")); 
+  procsDst_D0.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2640) #rightarrow D^{*+}", Process::Type::background, colors("red"),
+                   set<string>({ntpDst_D0}), globalCuts + " && truthmatch%100 == 20")); 
+  procsDst_D0.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2737) #rightarrow D^{*+}", Process::Type::background, colors("green"),
+                   set<string>({ntpDst_D0}), globalCuts + " && truthmatch%100 == 30")); 
+  procsDst_D0.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2978) #rightarrow D^{*+}", Process::Type::background, colors("darkblue"),
+                   set<string>({ntpDst_D0}), globalCuts + " && truthmatch%100 == 40")); 
+
+  vector<shared_ptr<Process> > procsDst0;
+  procsDst0.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2580) #rightarrow D^{*0}", Process::Type::background, colors("purple"),
+                   set<string>({ntpDst0}), globalCuts + " && truthmatch%100 == 10")); 
+  procsDst0.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2640) #rightarrow D^{*0}", Process::Type::background, colors("red"),
+                   set<string>({ntpDst0}), globalCuts + " && truthmatch%100 == 20")); 
+  procsDst0.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2737) #rightarrow D^{*0}", Process::Type::background, colors("green"),
+                   set<string>({ntpDst0}), globalCuts + " && truthmatch%100 == 30")); 
+  procsDst0.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2978) #rightarrow D^{*0}", Process::Type::background, colors("darkblue"),
+                   set<string>({ntpDst0}), globalCuts + " && truthmatch%100 == 40")); 
+
+  vector<shared_ptr<Process> > procsDsts;
+  procsDsts.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2580) #rightarrow D^{*0}", Process::Type::background, colors("purple"),
+                   set<string>({ntpDst0}), globalCuts + " && truthmatch%100 == 10")); 
+  procsDsts.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2640) #rightarrow D^{*0}", Process::Type::background, colors("red"),
+                   set<string>({ntpDst0}), globalCuts + " && truthmatch%100 == 20")); 
+  procsDsts.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2737) #rightarrow D^{*0}", Process::Type::background, colors("green"),
+                   set<string>({ntpDst0}), globalCuts + " && truthmatch%100 == 30")); 
+  procsDsts.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2978) #rightarrow D^{*0}", Process::Type::background, colors("darkblue"),
+                   set<string>({ntpDst0}), globalCuts + " && truthmatch%100 == 40")); 
+  procsDsts.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2580) #rightarrow D^{*+}", Process::Type::background, colors("purple"),
+                   set<string>({ntpDst_D0}), globalCuts + " && truthmatch%100 == 10",2)); 
+  procsDsts.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2640) #rightarrow D^{*+}", Process::Type::background, colors("red"),
+                   set<string>({ntpDst_D0}), globalCuts + " && truthmatch%100 == 20",2)); 
+  procsDsts.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2737) #rightarrow D^{*+}", Process::Type::background, colors("green"),
+                   set<string>({ntpDst_D0}), globalCuts + " && truthmatch%100 == 30",2)); 
+  procsDsts.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2978) #rightarrow D^{*+}", Process::Type::background, colors("darkblue"),
+                   set<string>({ntpDst_D0}), globalCuts + " && truthmatch%100 == 40",2)); 
 
      
   PlotMaker pm;
 
   // 
-  pm.Push<Hist1D>(Axis(50,2000,3000, "d_iso1_m", "m(D^{0}#pi) [GeV]"), "wskim_1os", procs, linplot);
-  pm.Push<Hist1D>(Axis(50,2100,3500, "d_iso1_iso2_m", "m(D^{0}#pi) [GeV]"), "wskim_2os", procs, linplot);
+  //pm.Push<Hist1D>(Axis(50,2000,3000, "d_iso1_m", "m(D^{0}#pi) [GeV]"), "wskim_1os", procs, linplot);
+  pm.Push<Hist1D>(Axis(50,2100,3500, "d_iso1_iso2_m", "m(D^{0}#pi#pi) [GeV]"), "wskim_2os", procs, linplot).Tag("D0");
+  pm.Push<Hist1D>(Axis(50,2100,3500, "d_iso1_iso2_m", "m(D^{*+}#pi#pi) [GeV]"), "wskim_2os", procsDst, linplot).Tag("Dst");
+  pm.Push<Hist1D>(Axis(50,2100,3500, "d_iso1_iso2_m", "m(D^{0}#pi#pi) [GeV]"), "wskim_2os", procsDst0, linplot).Tag("Dst0");
+  pm.Push<Hist1D>(Axis(50,2100,3500, "d_iso1_iso2_m", "m(D^{0}#pi#pi) [GeV]"), "wskim_2os", procsDst_D0, linplot).Tag("Dst_D0");
+  pm.Push<Hist1D>(Axis(40,2100,3500, "d_iso1_iso2_m", "m(D^{0}#pi#pi) [GeV]"), "wskim_2os", procsDsts, linplot).Tag("Dsts");
 
 
   pm.min_print_ = true;
