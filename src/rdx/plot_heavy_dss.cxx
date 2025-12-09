d572b435280a6a27618d2470808ff9ee919e5953
Manuel Franco Sevilla <manuelf@umd.edu>
2025-12-09 07:59:52 -0500

    Improved heavy D** plots

--- src/rdx/plot_heavy_dss.cxx
+++ src/rdx/plot_heavy_dss.cxx
@@ -31,7 +31,7 @@ int main(){
 
   // Plot types
   PlotOpt log_lumi("txt/plot_styles.txt", "LHCbPaper");
-  log_lumi.Title(TitleType::data)
+  log_lumi.Title(TitleType::info)
     .Bottom(BottomType::off)
     .YAxis(YAxisType::log)
     .Stack(StackType::data_norm)
@@ -59,26 +59,41 @@ int main(){
 
   string globalCuts = "mu_ubdt_ok && (k_p < 200) && (pi_p < 200) && (mu_p < 100) && (iso_p1 < 200) && (iso_p2 < 200) && (iso_p3 < 200) && (nspdhits < 450)";
 
-  string ntuple = "ntuples/0.9.17-all_years/2016/Dstst_heavy/DststHMuDst0-12875440-MagUp/D0--25_08_19--mc--12875440--2016--mu--tracker_only.root";
+  string ntpD0 = "ntuples/0.9.17-all_years/2016/Dstst_heavy/DststHMuD0-12675011-MagDown/D0--25_12_02--mc--12675011--2016--md--tracker_only.root";
+  //string ntpDst = "ntuples/0.9.17-all_years/2016/Dstst_heavy/DststHMuDst-11676012-MagDown/Dst--25_12_02--mc--11676012--2016--md--tracker_only.root";
+  string ntpDst = "ntuples/0.9.17-all_years/2016/Dstst_heavy/DststHMuDst-11676012-MagDown/D0--25_12_02--mc--11676012--2016--md--tracker_only.root";
   vector<shared_ptr<Process> > procs;
   procs.push_back(Process::MakeShared<Baby_rdx917>
-                  ("D**(2580)", Process::Type::background, colors("purple"),
-                   set<string>({ntuple}), globalCuts + " && truthmatch%100 == 10")); 
+                  ("D**(2580) #rightarrow D^{0}", Process::Type::background, colors("purple"),
+                   set<string>({ntpD0}), globalCuts + " && truthmatch%100 == 10")); 
   procs.push_back(Process::MakeShared<Baby_rdx917>
-                  ("D**(2640)", Process::Type::background, colors("red"),
-                   set<string>({ntuple}), globalCuts + " && truthmatch%100 == 20")); 
+                  ("D**(2640) #rightarrow D^{0}", Process::Type::background, colors("red"),
+                   set<string>({ntpD0}), globalCuts + " && truthmatch%100 == 20")); 
   procs.push_back(Process::MakeShared<Baby_rdx917>
-                  ("D**(2737)", Process::Type::background, colors("green"),
-                   set<string>({ntuple}), globalCuts + " && truthmatch%100 == 30")); 
+                  ("D**(2737) #rightarrow D^{0}", Process::Type::background, colors("green"),
+                   set<string>({ntpD0}), globalCuts + " && truthmatch%100 == 30")); 
   procs.push_back(Process::MakeShared<Baby_rdx917>
-                  ("D**(2978)", Process::Type::background, colors("darkblue"),
-                   set<string>({ntuple}), globalCuts + " && truthmatch%100 == 40")); 
+                  ("D**(2978) #rightarrow D^{0}", Process::Type::background, colors("darkblue"),
+                   set<string>({ntpD0}), globalCuts + " && truthmatch%100 == 40")); 
+  procs.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2580) #rightarrow D^{*+}", Process::Type::background, colors("purple"),
+                   set<string>({ntpDst}), globalCuts + " && truthmatch%100 == 10",2)); 
+  procs.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2640) #rightarrow D^{*+}", Process::Type::background, colors("red"),
+                   set<string>({ntpDst}), globalCuts + " && truthmatch%100 == 20",2)); 
+  procs.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2737) #rightarrow D^{*+}", Process::Type::background, colors("green"),
+                   set<string>({ntpDst}), globalCuts + " && truthmatch%100 == 30",2)); 
+  procs.push_back(Process::MakeShared<Baby_rdx917>
+                  ("D**(2978) #rightarrow D^{*+}", Process::Type::background, colors("darkblue"),
+                   set<string>({ntpDst}), globalCuts + " && truthmatch%100 == 40",2)); 
 
      
   PlotMaker pm;
 
   // 
-  pm.Push<Hist1D>(Axis(100,2000,3000, "d_iso1_m", "m(D^{0}#pi) [GeV]"), "wskim_1os", procs, linplot);
+  pm.Push<Hist1D>(Axis(50,2000,3000, "d_iso1_m", "m(D^{0}#pi) [GeV]"), "wskim_1os", procs, linplot);
+  pm.Push<Hist1D>(Axis(50,2100,3500, "d_iso1_iso2_m", "m(D^{0}#pi) [GeV]"), "wskim_2os", procs, linplot);
 
 
   pm.min_print_ = true;
